4aa8d6cb0d66d66b8ffdd30ce0b0e470336ae92c
Mehak bansal <mehak1921@yahoo.com>
2024-12-04 02:04:15 +0530

    solve day 1

---
+++ 2024/1/2/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const leftArr = []
+const rightArr = []
+
+
+// Function to read a file line by line
+function readFileLineByLine(filePath) {
+   const stream = fs.createReadStream(filePath);
+   const rl = readline.createInterface({
+      input: stream,
+      output: process.stdout,
+      terminal: false
+   });
+
+   rl.on('line', (line) => {
+      const str = line.replace("   ", " ").split(" ")
+      leftArr.push(str[0])
+      rightArr.push(str[1])
+   });
+
+   rl.on('close', () => {
+      const sortedLeftArr = leftArr.sort();
+      const sortedRightArr = rightArr.sort();
+
+      const tmpArr = []
+      for (i = 0; i < leftArr.length; ++i) {
+         tmpArr.push(sortedLeftArr[i] * sortedRightArr.filter(item => item === sortedLeftArr[i]).length);
+      }
+
+      console.log(tmpArr.reduce((prev, current) => prev + current, 0))
+   });
+}
+
+// Call the function with a file path
+readFileLineByLine('sample.txt');
+
